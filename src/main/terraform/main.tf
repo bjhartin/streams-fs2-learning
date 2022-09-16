47e5d2100d4eda246a5cca9ae2bc99a1ab18dc70
Brian Hartin <HartinBrian@johndeere.com>
2022-09-16 08:06:59 -0500

    Adding ALB terraform module

--- src/main/terraform/main.tf
+++ src/main/terraform/main.tf
@@ -44,3 +44,13 @@ module "security_groups" {
   container_port = var.container_port
 }
 
+module "alb" {
+  source              = "./modules/alb"
+  name                = var.name
+  vpc_id              = module.vpc.id
+  subnets             = module.vpc.public_subnets
+  environment         = var.environment
+  alb_security_groups = [module.security_groups.alb]
+  //  alb_tls_cert_arn    = var.tsl_certificate_arn
+  health_check_path   = var.health_check_path
+}
